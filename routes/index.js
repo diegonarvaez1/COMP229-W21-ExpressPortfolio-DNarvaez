9c2fad0d171e775120556d3f6bd553bb3aeead26
Diego Narvaez <diegonarva95@gmail.com>
2021-02-15 12:03:29 -0500

    added required pages for the project

--- routes/index.js
+++ routes/index.js
@@ -15,13 +15,13 @@ router.get('/', function(req, res, next) {
    /* GET about page. */
 router.get('/about', function(req, res, next) {
   res.render('index',
-   { title: 'About' });
+   { title: 'About Me' });
   });
 
    /* GET products page. */
-router.get('/products', function(req, res, next) {
+router.get('/projects', function(req, res, next) {
   res.render('index',
-   { title: 'Products' });
+   { title: 'Projects' });
   });
 
    /* GET services page. */
